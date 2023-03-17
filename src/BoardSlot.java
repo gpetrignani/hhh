ef82a18b3dcc5f58b4e32c7aa5c8fcd8ce198a05
Alberto Mosconi <albertomaria.mosconi@gmail.com>
2023-03-17 18:42:00 +0100

    optimize over 9000

--- src/BoardSlot.java
+++ src/BoardSlot.java
@@ -1,14 +1,5 @@
 public class BoardSlot {
-
-    /*private boolean inOut;
-    private boolean itemTile;
-    private boolean threePlayerSlot;
-    private boolean fourPlayerSlot;
-     */
-
-
-
-    private TileType type;
+    private final TileType type;
     private ItemTile Item;
 
     public BoardSlot (TileType Type){
@@ -17,51 +8,6 @@ public class BoardSlot {
 
     public TileType getType() {
         return type;
-    }
-
-
-
-
-
-
-
-
-
-    public void BoardSlotInOut (boolean inOut){
-
-        this.inOut = inOut;
-    }
-
-    public void itemTilePresent (boolean itemTile){
-
-        this.itemTile = itemTile;
-    }
-
-    public void threePlayerSlotGame (boolean threePlayerSlot){
-
-        this.threePlayerSlot = threePlayerSlot;
-    }
-
-    public void fourPlayerSlotGame (boolean fourPlayerSlot){
-
-        this.fourPlayerSlot = fourPlayerSlot;
-    }
-
-
-    public boolean isInOut() {
-        return inOut;
-    }
-
-    public boolean isItemTile() {
-        return itemTile;
-    }
-
-    public boolean isThreePlayerSlot() {
-        return threePlayerSlot;
-    }
-
-    public boolean isFourPlayerSlot() {
-        return fourPlayerSlot;
     }
 }
 
